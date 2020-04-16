1efb136e423f13ace2f52305d854bd14bf0cc6b9
Gabriel Teixeira <gabriel.teixeira94@gmail.com>
2020-04-16 14:16:45 -0300

    refactoring process

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ server.set("view engine", "njk")
 server.use(express.static('public'))
 
 nunjucks.configure("views", {
-    express:server
+    express: server,
+    autoescape: false,
+    noCache: true
 })
 
 server.get("/", function(req, res) {
@@ -21,6 +23,10 @@ server.get("/about", function(req, res) {
 
 server.get("/recipes", function(req, res) {
     return res.render("recipes")
+})
+
+server.get("/detail", function(req, res) {
+    return res.render("detail")
 })
 
 server.listen(5000, function(){
