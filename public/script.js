1efea83472855e6e45c765bca95c2e584dd2db3c
Gabriel Teixeira <gabriel.teixeira94@gmail.com>
2020-05-03 21:51:46 -0300

    refactoring finished - admin styling completed

--- public/script.js
+++ public/script.js
@@ -33,7 +33,9 @@ function addIngredient(){
     const newField = fieldContainer[fieldContainer.length - 1].cloneNode(true);
 
     if (newField.children[0].value == "") return false;
+
     newField.children[0].value = "";
+
     ingredients.appendChild(newField);
 }
 
@@ -44,9 +46,11 @@ function addStep(){
     const fieldContainer = document.querySelectorAll(".step");
     
     const newField = fieldContainer[fieldContainer.length - 1].cloneNode(true);
-
+    
     if (newField.children[0].value == "") return false;
+
     newField.children[0].value = "";
+   
     steps.appendChild(newField);
 }
 
